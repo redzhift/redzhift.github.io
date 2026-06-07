ebcd19448d58e763498ce9088f3c085310f4405d
Jin <44684891+mghwajin@users.noreply.github.com>
2026-06-06 17:12:32 -0700

    deploy hugo

---
+++ content/posts/test-post.md
@@ -0,0 +1,13 @@
++++
+date = '2026-06-06T16:46:47-07:00'
+draft = false
+title = 'Test Post'
++++
+
+## Introduction
+
+This is **bold** text, and this is *emphasized* text.
+
+Visit my portfolio [here][link]!
+
+[link]: https://redzhift.atlassian.net/wiki/spaces/~jcho/overview
---
+++ public/searchindex.en.js
@@ -0,0 +1,50 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Jin C.",
+    "content": "Well met! 🛰️✨ My name is Jin.\nMy portfolio is available in Confluence while construction here is underway. Cheers!\n🧠 Currently learning… Full stack fundamentals via JavaScript\nHugo\n🔧 Currently tinkering with… Atlassian tools - Confluence, Jira, Trello\n💡 Currently working on… Static site portfolio\nHTML/CSS review\nSam-Il (삼일) - 3rd dan black belt pattern in ITF Taekwondo\n📫 Reach me at… redzhift@tuta.io\nor\nlinkedin.com/in/redzhift",
+    "description": "Well met! 🛰️✨ My name is Jin.\nMy portfolio is available in Confluence while construction here is underway. Cheers!\n🧠 Currently learning… Full stack fundamentals via JavaScript\nHugo\n🔧 Currently tinkering with…",
+    "tags": [],
+    "title": "About",
+    "uri": "/about/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Jin C.",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Jin C.",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "Jin C. \u003e Posts",
+    "content": "Introduction This is bold text, and this is emphasized text.\nVisit my portfolio here!",
+    "description": "Introduction This is bold text, and this is emphasized text.\nVisit my portfolio here!",
+    "tags": [],
+    "title": "Test Post",
+    "uri": "/posts/test-post/index.html"
+  },
+  {
+    "breadcrumb": "Jin C.",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Jin C.",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
